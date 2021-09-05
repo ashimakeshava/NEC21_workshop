4bf1a9857152ba94abb0c5750b8a05483281c20f
Linus <linus-tiemann@live.de>
2021-09-05 19:24:46 +0200

    display for hit infos and raycast All

--- NEC21_eyetracking unity/Assets/Scripts/LaserPointer.cs
+++ NEC21_eyetracking unity/Assets/Scripts/LaserPointer.cs
@@ -1,20 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LaserPointer : MonoBehaviour
 {
-    public bool ShowHit;
     public bool drawRay;
+    public bool RayCastAll;
+    public bool showInfo;
+    
     public float maxDistance = 3f;
     public GameObject origin;
     public GameObject TargetEffect;
     public LineRenderer lineRenderer;
-
+    public TVScreen TVScreen;
 
     private bool hitTarget;
     private List<GameObject> hitTargets;
+    
     
     
     // Start is called before the first frame update
@@ -27,20 +31,48 @@ public class LaserPointer : MonoBehaviour
     void Update()
     {
         
-        RaycastHit hitInfo;
+        RaycastHit hitInfo =new RaycastHit();
+        RaycastHit[] hitInfos = new RaycastHit[] { };
 
-        if (Physics.Raycast(origin.transform.position, origin.transform.up, out hitInfo, maxDistance))
+        if (!RayCastAll)
         {
-            hitTarget = true;
+            
+            if (Physics.Raycast(origin.transform.position, origin.transform.up, out hitInfo, maxDistance))
+            {
+                hitTarget = true;
+            }
+            else
+            {
+                hitTarget = false;
+            }
         }
         else
         {
-            hitTarget = false;
+            hitInfos = Physics.RaycastAll(origin.transform.position, origin.transform.up, maxDistance);
+            
+            //sort hit infos by distance
+            
+            if (hitInfos.Length != 0)
+            {
+                hitTarget=true;
+                
+                //Order by distance
+                hitInfos = hitInfos.OrderBy(x => x.distance).ToArray();
+                hitInfo = hitInfos[hitInfos.Length - 1];
+            }
+            else
+            {
+                hitTarget = false;
+            }
+            
+            
         }
-        
-        
-        
-        
+
+
+
+
+
+
         //render Ray
         lineRenderer.gameObject.SetActive(drawRay);
         TargetEffect.SetActive(drawRay);
@@ -56,10 +88,24 @@ public class LaserPointer : MonoBehaviour
             }
         }
         
-        
-        
-        
-        
+        //show information on Screen
+
+        if (showInfo)
+        {
+            if (hitTarget)
+            {
+                if (RayCastAll)
+                {
+                    Debug.Log(hitInfos.Length);
+                    TVScreen.DisplayInfo(hitInfos.ToList());
+                }
+                    
+                else
+                {
+                    TVScreen.DisplayInfo(hitInfo);
+                }  
+            }
+        }
     }
 
 
